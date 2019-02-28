1381928fb7711b32924299b7f2aee2b91d28cc63
Anthony PERIQUET <anthony@periquet.net>
2019-02-28 16:30:03 +0100

    Fix : escape asterisk ...

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -9,6 +9,6 @@ command="/opt/scripts/backup.job.sh ${@} > /proc/1/fd/1 2>&1"
 
 echo "Job to be executed : ${command}"
 # Configure the job
-crontab -l | { cat; echo ${CRONFIG} ${command}; } | crontab -
+crontab -l | { cat; echo "${CRONFIG} ${command}"; } | crontab -
 # Run the job
 crond -l 2 -f
